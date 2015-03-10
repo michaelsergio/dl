490272dfa762a245af81072fceee815f0732ac23
Michael Sergio <mikeserg@gmail.com>
2015-03-10 19:29:36 -0400

    Added IOCTL for terminal size.

--- hr.c
+++ hr.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
 
 typedef struct {
   unsigned int columns;
 } line_options_t;
+
+unsigned int getColumnWidthFromOS() {
+  struct winsize winsz;
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, &winsz);
+  return (unsigned int) winsz.ws_col;
+}
 
 void drawLine(line_options_t *options) {
   for (unsigned int i = 0; i < options->columns; i++)  {
@@ -16,6 +24,8 @@ int main(int argc, const char *argv[])
   line_options_t options = { 
     .columns=80 
   };
+
+  options.columns = getColumnWidthFromOS();
 
   drawLine(&options);
 
