d24d336428fecd67e4311784dff115b1e40e066d
Michael Sergio <mikeserg@gmail.com>
2015-03-27 18:35:51 -0400

    Fixed new line bug. Stupid puts instead of printf.

--- hr.c
+++ hr.c
@@ -83,7 +83,7 @@ void drawLine(line_options_t *options) {
   size_t position = 0;
 
   if (options->color) {
-    puts(options->color);
+    printf("%s", options->color);
   }
 
   for (i = 0; i < options->columns; i++)  {
@@ -93,7 +93,7 @@ void drawLine(line_options_t *options) {
   }
 
   if (options->color) {
-    puts(ANSI_COLOR_RESET);
+    printf("%s", ANSI_COLOR_RESET);
   }
   putchar('\n');
 
